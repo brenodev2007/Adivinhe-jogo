55af85932b89741af7e6044112aa4bd311d63e03
Breno Soriani <breno.soriani@gmail.com>
2025-07-14 10:45:21 -0300

    Deixando Header Dinâmica

--- primeiro-projeto/src/App.tsx
+++ primeiro-projeto/src/App.tsx
@@ -2,9 +2,13 @@ import { Header } from "./components/Header";
 import styles from "./app.module.css";
 
 function App() {
+  function handleRestartGame() {
+    alert("Reinicar o Jogo!");
+  }
+
   return (
     <div className={styles.container}>
-      <Header />
+      <Header current={5} max={10} onRestart={handleRestartGame} />
     </div>
   );
 }
