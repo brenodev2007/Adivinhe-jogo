06ead01c76fd64ddaffb987af3b48c5199597be0
Breno Soriani <breno.soriani@gmail.com>
2025-07-14 11:41:49 -0300

    infra

--- primeiro-projeto/src/App.tsx
+++ primeiro-projeto/src/App.tsx
@@ -10,17 +10,19 @@ function App() {
 
   return (
     <div className={styles.container}>
-      <Header current={5} max={10} onRestart={handleRestartGame} />
+      <main>
+        <Header current={5} max={10} onRestart={handleRestartGame} />
 
-      <Tip tip="Linguagem de programação dinâmica" />
+        <Tip tip="Linguagem de programação dinâmica" />
 
-      <div className={styles.word}>
-        <Letter value="R" />
-        <Letter value="R" />
-        <Letter value="R" />
-        <Letter value="R" />
-        <Letter value="R" />
-      </div>
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="R" />
+          <Letter value="R" />
+          <Letter value="R" />
+          <Letter value="R" />
+        </div>
+      </main>
     </div>
   );
 }
