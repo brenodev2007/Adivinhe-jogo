8449e2f6880fd77dff0a7327c3c89951d335cbef
Breno Soriani <breno.soriani@gmail.com>
2025-07-15 08:36:29 -0300

    Listando letras utilizadas

--- primeiro-projeto/src/App.tsx
+++ primeiro-projeto/src/App.tsx
@@ -1,39 +1,64 @@
+import { useEffect, useState } from "react";
+import styles from "./app.module.css";
+
 import { Header } from "./components/Header";
 import { Tip } from "./components/Tip";
 import { Letter } from "./components/Letter";
 import { Input } from "./components/Input";
 import { Button } from "./components/Button";
-import { LettersUsed } from "./components/LettersUsed";
-import styles from "./app.module.css";
+import { LettersUsed, type LettersUsedProps } from "./components/LettersUsed";
+
+import { WORDS } from "./utils/words";
+import type { Challenge } from "./utils/words";
 
 function App() {
+  const [attemps, setScore] = useState(0);
+  const [letter, setLetter] = useState("");
+  const [lettersUsed, setLetterUsed] = useState<LettersUsedProps[]>([]);
+  const [challenge, setChallenge] = useState<Challenge | null>(null);
+
   function handleRestartGame() {
-    alert("Reinicar o Jogo!");
+    alert("Reiniciar o Jogo!");
+  }
+
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length);
+    const word = WORDS[index];
+    setChallenge(word);
+    setLetter("");
+    setScore(0);
+    setLetterUsed([]);
+  }
+
+  useEffect(() => {
+    startGame();
+  }, []);
+
+  if (!challenge) {
+    return <div>Carregando...</div>;
   }
 
   return (
     <div className={styles.container}>
       <main>
-        <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Header current={attemps} max={10} onRestart={handleRestartGame} />
 
         <Tip tip="Linguagem de programação dinâmica" />
 
         <div className={styles.word}>
-          <Letter value="R" />
-          <Letter value="E" />
-          <Letter value="a" />
-          <Letter value="c" />
-          <Letter value="t" />
+          {challenge.word.split("").map((_, index) => (
+            <Letter key={index} value="" />
+          ))}
         </div>
 
         <h4>Palpite</h4>
 
         <div className={styles.gap}>
-          <Input autoFocus maxLength={1} placeholder="?"></Input>
+          <Input autoFocus maxLength={1} placeholder="?" />
           <Button title="Confirmar" />
         </div>
 
-        <LettersUsed />
+        <LettersUsed data={lettersUsed} />
       </main>
     </div>
   );
